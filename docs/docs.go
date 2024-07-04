b11f3d989b7c8fc7c29349c1f7e47dcf3845802a
Ростислав Жуков <rosf.zhukov@gmail.com>
2024-07-04 19:54:38 +0300

    docs update

--- docs/docs.go
+++ docs/docs.go
@@ -757,7 +757,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "http://localhost:8080",
+	Host:             "localhost:8080",
 	BasePath:         "",
 	Schemes:          []string{"http"},
 	Title:            "Paste API",
